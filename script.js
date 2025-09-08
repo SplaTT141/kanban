bc1ee37d0a518ea15504c041b55f4678f6e2874a
Ernest <zyperis14@gmail.com>
2025-09-08 12:30:02 +0300

    new task + task removal

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const inputDOM = document.getElementById('add-input');
 const addBtnDOM = document.getElementById('add-btn');
-const taskDOM = document.querySelector('.task');
+const tasksDOM = document.querySelector('.tasks');
 
 addBtnDOM.addEventListener('click', () => {
-    taskDOM.innerHTML = `
-         <p class="text">${inputDOM.value}</p>
-         <div class="task-action">
-             <button class="edit fa fa-pencil"></button>
-             <button class="remove fa fa-trash"></button>
-         </div>
-    `;
+
+    if (inputDOM.value !== '') {
+        tasksDOM.insertAdjacentHTML('afterbegin', `
+            <div class="task">
+                <p class="text">${inputDOM.value}</p>
+                <div class="task-action">
+                    <button class="edit fa fa-pencil"></button>
+                    <button class="remove fa fa-trash"></button>
+                </div>
+            </div>`);
+    }
+
+    tasksDOM.addEventListener('click', e => {
+        if (e.target.classList.contains('remove')) {
+            e.target.closest('.task').remove();
+        }
+    })
+
+    inputDOM.value = '';
 });
